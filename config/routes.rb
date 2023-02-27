95a70ed083a6fa618bd0d51f549aa493ffa98aa1
jane eme <janeeme23@gmail.com>
2023-02-27 09:33:45 +0100

    airbnb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  # get 'flats', to: 'flats#index'
+  # post 'flats', to: 'flats#create'
+  # get 'flats/new', to: 'flats#new', as: :new_flat
+  # get 'flats/:id', to: 'flats#show', as: :flat
+  # get 'flats/:id/edit', to: 'flats#edit', as: :edit_flat
+  # patch 'flats/:id', to: 'flats#update'
+  # delete 'flats/:id', to: 'flats#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "flats#index"
+  resources :flats, only:[:index, :new, :create, :show]
 end
